3babfacd17fbf2700d69b64c49eb9bf510465c7f
aqilahJ925 <nuraqj925@gmail.com>
2025-12-18 05:03:51 +0800

    database admin

---
+++ Database/storagedb.sql
@@ -0,0 +1,56 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 15, 2025 at 02:49 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `storagedb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_cred`
+--
+
+CREATE TABLE `admin_cred` (
+  `sr_no` int(5) NOT NULL,
+  `admin_name` varchar(30) NOT NULL,
+  `admin_pass` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin_cred`
+--
+
+INSERT INTO `admin_cred` (`sr_no`, `admin_name`, `admin_pass`) VALUES
+(1, 'kiki', '12345');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin_cred`
+--
+ALTER TABLE `admin_cred`
+  ADD PRIMARY KEY (`sr_no`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
